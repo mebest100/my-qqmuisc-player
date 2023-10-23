41db077006413457f06b13799223b006ec5910da
yuanadmin <yuanadmin@yuan.com>
2023-10-23 16:21:43 +0800

    achieve get banner and get recomend songlist in jsosoApi codes

--- src/registerRouter/jsosoApi/routes.js
+++ src/registerRouter/jsosoApi/routes.js
@@ -50,53 +50,43 @@ const RegisterGetBanner = (app) => {
 
 const RegisterGetRecomendDiscList = (app) => {
     app.get('/api/getRecomendDiscList', async (req, res) => {
-        const {
-            raw,
-            pageNo = 1,
-            pageSize = 20,
-            id = 3317, // 3317: 官方歌单，59：经典，71：情歌，3056：网络歌曲，64：KTV热歌
-        } = req.query;
-
-        const data = {
-            data: JSON.stringify({
-                comm: {
-                    ct: 24
-                },
-                playlist: {
-                    method: "get_playlist_by_category",
-                    param: {
-                        id: id / 1,
-                        curPage: pageNo / 1,
-                        size: pageSize / 1,
-                        order: 5,
-                        titleid: id / 1,
-                    },
-                    module: "playlist.PlayListPlazaServer"
-                }
-            }),
-        };
 
         return new Promise(async (resolve, reject) => {
             try {
+                const { raw } = req.query;
+
+                const data = {
+                    data: JSON.stringify({
+                        comm: {
+                            ct: 24
+                        },
+                        recomPlaylist: {
+                            method: "get_hot_recommend",
+                            param: {
+                                async: 1,
+                                cmd: 2
+                            },
+                            module: "playlist.HotRecommendServer"
+                        }
+                    })
+                };
+
                 const result = await request({
                     url: 'http://u.y.qq.com/cgi-bin/musicu.fcg',
                     data,
                 });
 
                 if (Number(raw)) {
-                    return resolve(res.json({ result }));
+                    res.send(result);
                 } else {
-                    const { total, v_playlist } = result.playlist.data;
-                    return resolve(res.json({
+                    const list = result.recomPlaylist.data.v_hot;
+                    res.send({
                         result: 100,
                         data: {
-                            total,
-                            list: v_playlist,
-                            id,
-                            pageNo,
-                            pageSize,
+                            list,
+                            count: list.length,
                         }
-                    }))
+                    })
                 }
 
             } catch (error) {
